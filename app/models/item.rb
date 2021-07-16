c3c07814a49a57269039d15b5265aa8927f7ff06
aririn712 <atnil012@icloud.com>
2021-07-16 13:54:25 +0900

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,16 +8,21 @@ class Item < ApplicationRecord
   belongs_to :number
   has_one_attached :image
 
-  validates :image, presence: true
-  validates :title, presence: true
-  validates :description, presence: true
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :status_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :bearer_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :number_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :price, presence: true, format: { with: /\A[0-9]+\z/ },
+  with_options presence: true do
+    validates :image
+    validates :title
+    validates :description
+  end
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category_id
+    validates :status_id
+    validates :bearer_id
+    validates :area_id
+    validates :number_id
+  end
+  validates :price, presence: true, format: { with: /\A[0-9]+\z/, allow_blank: true },
                     numericality: {
+                      allow_blank: true,
                       only_integer: true,
                       greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999
                     }
